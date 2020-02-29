1e04c0d814bc7803663a84ff7ee2550701cd3af1
kneubel2 <kneubel2@illinois.edu>
2020-02-29 04:47:15 -0600

    recipe parser works great :zap:

--- scraper.py
+++ scraper.py
@@ -1,71 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 import heapq
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 
 
 def worm(starting_URL):
-	http = urllib3.PoolManager()
 	URLs = [starting_URL]
 	visited = set()
 	visited.add(starting_URL)
 
+	ingredients_list = open("ingredients.txt","a")
+	lists_written = 0
+
 	while len(URLs) != 0:
 		URL = URLs.pop()
 
-		# print the recipe:
-		response = http.request('GET',URL)
-		html_file = response.data
+		# start of brennen code
+		ingredients = []
+		result = requests.get(URL)
+		src = result.content
 
-		try:
+		soup = BeautifulSoup(src, 'lxml')
+		lists = soup.find_all("ul")
 
-			tag = "\"recipeIngredient\": "
-
-			for c_in in range(len(html_file) - len(tag)):
-				if html_file[c_in : c_in + len(tag) ] == tag.encode('ASCII'):
-					break
-			ingredients_list = []
-			cur_str = ''
-			begin = 0
-
-			for index in range(c_in + len(tag), len(html_file) - 1):
-				# if the end of an ingredient, add the ingredient and move index past quotation and comma
-				if chr(html_file[index]) + chr(html_file[index + 1]) == '\",':
-					ingredients_list.append(cur_str)
-					cur_str = ''
-					index += 2
-				cur_str += chr(html_file[index])
-				# if the end of the entire list, add the final ingredient, chop off the quotation and ]
-				if(chr(html_file[index]) == ']'):
-					ingredients_list.append(cur_str)
-					break
-			# clean up ingredients and append to final list
-			final_list = []
-			for ingredient in ingredients_list:
-				ingredient = ingredient.strip('\n')
-				for index in range(len(ingredient)):
-					if ingredient[index] == '\"':
-						break
-				final_list.append(ingredient[index + 1 :])
-			# fix crap at end of last ingredient
-			if len(final_list) >= 1:
-				last_word = final_list[-1]
-				for char_i in range(len(last_word)):
-					if last_word[char_i] == '\"':
-						break
-				final_list[-1] = last_word[:char_i]
-
-			if len(final_list) != 0:
-				print(final_list)
-			else:
-				print("didnt work for this url")
-		except:
-			print("didnt work for this url")
+		for ls in lists:
+			try:
+				if "lst_ingredients" in ls.attrs['id']:
+					for ingredient in ls.find_all("span"):
+						if ingredient.attrs['itemprop'] == 'recipeIngredient':
+							ingredients.append(ingredient.get_text())
+			except:
+				continue
+		if len(ingredients) != 0:
+			lists_written += 1
+			print(f"Recipes written: {lists_written}")
+			for ingredient in ingredients:
+				ingredients_list.write(ingredient + "~")
+			ingredients_list.write("\n")
 		# end of brennen code
-
 
 		page = requests.get(URL)
 		html = str(page.content)
